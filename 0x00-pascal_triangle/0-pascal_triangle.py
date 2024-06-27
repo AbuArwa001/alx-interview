37df1583c925491a54ee9a0ecb57d972182579aa
AbuArwa001 <khalfanathman12@yahoo.com>
2024-06-27 20:57:04 +0300

    Change approach

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,56 +4,21 @@ Pascal triangle
 """
 
 
-def generate_pattern(power):
-    """
-    Generate a row of Pascal's Triangle using binomial coefficients.
-
-    Args:
-    power (int): The row number of Pascal's Triangle to generate.
-
-    Returns:
-    list[int]: The generated row of Pascal's Triangle.
-    """
-    # Initialize an empty list to store the pattern
-    pattern = []
-
-    # Generate the pattern using binomial coefficients
-    for i in range(power + 1):
-        coefficient = binomial_coefficient(power, i)
-        pattern.append(coefficient)
-    return pattern
-
-
-def binomial_coefficient(n, k):
-    """
-    Calculate binomial coefficient (n choose k) using an iterative approach.
-
-    Args:
-    n (int): The number of items.
-    k (int): The number of items to choose.
-
-    Returns:
-    int: The binomial coefficient.
-    """
-    # Calculate the binomial coefficient "n choose k"
-    if k == 0 or k == n:
-        return 1
-    return binomial_coefficient(n - 1, k - 1) + binomial_coefficient(n - 1, k)
-
-
 def pascal_triangle(n):
     """
-    Create Pascal's Triangle up to the nth row.
-
-    Args:
-    n (int): The number of rows of Pascal's Triangle to generate.
-
-    Returns:
-    list[list[int]]: The generated Pascal's Triangle.
+    Returns a list of integers
+    representing the Pascal Triangle of n
+    returns empty list if n <= 0
     """
-    pascal_triangle = []
+    k = []
     if n <= 0:
-        return []
-    for i in range(n):
-        pascal_triangle.append(generate_pattern(i))
-    return pascal_triangle
+        return k
+    k = [[1]]
+    for i in range(1, n):
+        temp = [1]
+        for j in range(len(k[i - 1]) - 1):
+            curr = k[i - 1]
+            temp.append(k[i - 1][j] + k[i - 1][j + 1])
+        temp.append(1)
+        k.append(temp)
+    return k
