2756fe933a7fbd60de2cc2dcce6c3b39e48cf5fc
AbuArwa001 <khalfanathman12@yahoo.com>
2024-06-27 23:03:25 +0800

    Pascla triangle using binomial coefficient

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Pascal triangle
+"""
+def generate_pattern(power):
+    # Initialize an empty list to store the pattern
+    pattern = []
+
+    # Generate the pattern using binomial coefficients
+    for i in range(power + 1):
+        coefficient = binomial_coefficient(power, i)
+        pattern.append(coefficient)
+
+    return pattern
+
+def binomial_coefficient(n, k):
+    # Calculate the binomial coefficient "n choose k"
+    if k == 0 or k == n:
+        return 1
+    return binomial_coefficient(n - 1, k - 1) + binomial_coefficient(n - 1, k)
+
+
+    return pattern
+
+def pascal_triangle(n):
+    pascal_triangle = []
+    if n <= 0:
+        return
+    for i in range(n):
+        pascal_triangle.append(generate_pattern(i))
+    return pascal_triangle
