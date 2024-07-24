b706a8137bd07d079c682ecedae1292a7d5b1c26
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-25 00:55:47 +0300

    0-validate_utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -29,8 +29,10 @@ def validUTF8(data):
                 n_bytes = 2
             elif num >> 3 == 0b11110:
                 n_bytes = 3
-            elif num >> 7:
+            elif num >> 7 and len(data) == 1:
                 return True
+            elif num >> 7:
+                return False
         else:
             if not is_valid_byte(num, 0b11000000, 0b10000000):
                 return False
