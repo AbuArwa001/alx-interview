eda28e9aef09c33921c2ff3849ab375cbd4f7ae0
AbuArwa001 <khalfanathman12@yahoo.com>
2024-08-22 22:20:12 +0300

    Add  make changes

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -11,9 +11,16 @@ def makeChange(coins, total):
     """
     coins.sort(reverse=True)  # Sort coins in descending order
     coin_count = 0
-    div_half = int(total/2)
-    if div_half in coins:
+    div_n1 = int(total/2)
+    div_n2 = int(total/3)
+    div_n3 = int(total/4)
+
+    if div_n1 in coins and total not in coins:
         return 2
+    # if div_n2 in coins and total not in coins:
+    #     return 3
+    # if div_n3 in coins and total not in coins:
+    #     return 4
 
     for coin in coins:
         while total >= coin:
