8b4afe02cd700c09e7eb6350b229241c8b1e20a0
AbuArwa001 <khalfanathman12@yahoo.com>
2024-08-22 19:11:27 +0300

    make change for coins

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module To Give Change
+"""
+def makeChange(coins, total):
+    """
+    Given a pile of coins of different values,
+    determine the fewest number of coins needed to meet a given amount total.
+    """
+    remainder = total
+    coins_count = 0
+    if total == 0:
+        return 0
+    for coin in reversed(sorted(coins)):
+        while coin <= remainder:
+            remainder -= coin
+            coins_count += 1
+    if remainder == 0:
+        return coins_count
+    else:
+        return -1
